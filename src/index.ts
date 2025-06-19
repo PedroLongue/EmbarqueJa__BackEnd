5a1c0c6aea2ff2d6e292387505c5b97d06d12e1f
PedroLongue <pedrolongue13@gmail.com>
2025-06-19 09:45:11 -0300

    fix: adjust cors.

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://31.97.171.60:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://31.97.171.60:5173",
+      "http://embarqueja.xyz/",
+      "https://embarqueja.xyz/",
+      "http://www.embarqueja.xyz/",
+      "https://www.embarqueja.xyz/",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   },
 });
